ac2c48d461b9060dde3b903bc98b35c526fe7822
MohammadAyaz04 <mohdayaz12a70@gmail.com>
2025-06-30 10:58:55 +0000

    inheritance use

---
+++ 09_oopclasses/inheritance.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username) //pehle hamlog User.call(username) use krte the
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe() //Username is masalaChai
+
+console.log(chai instanceof User); //true also instanceof teacher is true
